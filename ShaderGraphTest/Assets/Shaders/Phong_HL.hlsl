ed9808b67e2ce5471bc49be3eab40708d1f8d751
Benjamin Gillott <bg357@cornell.edu>
2020-12-21 20:12:51 -0500

    rm intentional incorrect code from phong example

--- ShaderGraphTest/Assets/Shaders/Phong_HL.hlsl
+++ ShaderGraphTest/Assets/Shaders/Phong_HL.hlsl
@@ -7,50 +7,23 @@ void main(float3 vPosition, float4x4 uModel, float4x4 uView, float3 vNormal, flo
     float3 ambient = [.1, 0., 0.];
     float3 diffColor = [0.2, 0.8, 0.4];
     float3 specColor = [1.0, 1.0, 1.0];
-    
+
     // Correct phong model for reference
-    if (uRef == 1.) {
-        float3x3 modelMatrix = float3x3(uModel);
+    float3x3 modelMatrix = float3x3(uModel);
 
-        float3 worldPos = float3(uModel * float4(-vPosition, 1.));
-        float3 worldNorm = float3(uModel * float4(vNormal, 0.));
+    float3 worldPos = float3(uModel * float4(-vPosition, 1.));
+    float3 worldNorm = float3(uModel * float4(vNormal, 0.));
 
-        float3 lightDir = normalize(uLight - worldPos);
-        float lightWorldDot = dot(lightDir, worldNorm);
-        float diffuse = max(lightWorldDot, 0.);
+    float3 lightDir = normalize(uLight - worldPos);
+    float lightWorldDot = dot(lightDir, worldNorm);
+    float diffuse = max(lightWorldDot, 0.);
 
-        float3 camPos = float3(uView*float4(worldPos, 1.));
+    float3 camPos = float3(uView*float4(worldPos, 1.));
 
-        float3 reflectDir = float3(uView*float4(reflect(-lightDir, worldNorm), 0.));
+    float3 reflectDir = float3(uView*float4(reflect(-lightDir, worldNorm), 0.));
 
-        float specular = pow(max(dot(normalize(-camPos), reflectDir), 0.), 32.);
-        gl_FragColor = float4(ambient + diffuse * diffColor + specular * specColor, 1.);
-    }
-
-    // Example phong model with errors
-    else {        
-        float3x3 modelMatrix = float3x3(uModel);
-        //float3 worldPos = modelMatrix * vPosition;
-        //float3 worldNorm = modelMatrix * vNormal;
-
-        //float3 worldPos = float3(uModel * float4(vPosition, 1.));
-        //float3 worldNorm = float3(uModel * float4(vNormal, 1.));
-        // Uncomment for failing example
-        float3 worldPos = vPosition;
-        float3 worldNorm = vNormal;
-
-        float3 lightDir = normalize(uLight - worldPos);
-        float lightWorldDot = dot(lightDir, worldNorm);
-        float diffuse = max(lightWorldDot, 0.);
-
-        float3 camPos = float3(uView*float4(worldPos, 1.));
-
-        float3 reflectDir = float3(uView*float4(reflect(-lightDir, worldNorm), 0.));
-
-        float specular = pow(max(dot(normalize(-camPos), reflectDir), 0.), 32.);
-        gl_FragColor = float4(ambient + diffuse * diffColor + specular * specColor, 1.0);
-        // gl_FragColor = float4(ambient + diffuse * diffColor, 1.0);
-    }
+    float specular = pow(max(dot(normalize(-camPos), reflectDir), 0.), 32.);
+    gl_FragColor = float4(ambient + diffuse * diffColor + specular * specColor, 1.);
 }
 
 
